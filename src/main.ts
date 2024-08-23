86cc2e5a62c34920b7897ddf1e536e25bd97a58a
afk0901 <arnarfkr@gmail.com>
2024-08-23 19:41:53 +0100

    Buy me a coffee, code formatting and small cleaning

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import initializeValidation from "./formValidationPreventSubmit";
-import { calculateCompoundInterest, calculateVariance } from "./services/calculations";
+import {calculateCompoundInterest, calculateVariance} from "./services/calculations";
 import { getFloatValue, updateOutput } from "./services/formInputs";
 
 initializeValidation();
 
-const form: HTMLFormElement = document.getElementById(
-  "compoundInterestForm",
-) as HTMLFormElement;
+const form: HTMLFormElement = document.getElementById("compoundInterestForm") as HTMLFormElement;
 
 form.addEventListener("submit", handleFormSubmit);
 
@@ -14,23 +12,28 @@ function handleFormSubmit(event: Event): void {
   event.preventDefault();
 
   const principal: number = getFloatValue("principal");
-  const rate: number = getFloatValue("rate")
+  const rate: number = getFloatValue("rate");
   const compounding: number = getFloatValue("compounding");
   const time: number = getFloatValue("time");
-  const variance: number = getFloatValue("variance")
+  const variance: number = getFloatValue("variance");
   const finalAmount: number = calculateCompoundInterest(
     principal,
-    rate/100,
+    rate / 100,
+    compounding,
+    time
+  );
+
+  const calculatedVariance = calculateVariance(
+    principal,
+    rate,
     compounding,
     time,
-  );
-  
-  const finalAmountVarianceAbove: number = calculateVariance(principal, rate, compounding, time, variance).total_above
-  const finalAmountVarianceBelow: number = calculateVariance(principal, rate, compounding, time, variance).total_below
-
+    variance
+  )
+ 
   if (form.checkValidity() === true) {
     updateOutput("finalAmount", finalAmount);
-    updateOutput("finalAmountVarianceAbove", finalAmountVarianceAbove);
-    updateOutput("finalAmountVarianceBelow", finalAmountVarianceBelow);
+    updateOutput("finalAmountVarianceAbove", calculatedVariance.total_above);
+    updateOutput("finalAmountVarianceBelow", calculatedVariance.total_below);
   }
 }
