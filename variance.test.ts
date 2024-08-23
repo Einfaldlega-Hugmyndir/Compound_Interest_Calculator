f6bbe093c0d78794bce97baf2c0d73bb72ddec0e
afk0901 <arnarfkr@gmail.com>
2024-08-23 19:41:53 +0100

    Buy me a coffee, code formatting and small cleaning

--- variance.test.ts
+++ variance.test.ts
@@ -1,51 +1,47 @@
 import { calculateVariance } from "./src/services/calculations";
 
+const principal = 2500;
+const annualRate = 6;
+const years = 7;
+const compoundsPerYear = 12;
+const variance = 3;
+
 describe("calculate with variance", () => {
+  test("calculate with variance above", () => {
+    const variance_above = calculateVariance(
+      principal,
+      annualRate,
+      compoundsPerYear,
+      years,
+      variance
+    ).total_above;
+    expect(variance_above).toBe(4683.005);
+  });
 
-    test("calculate with variance above", () => {
-        const principal = 2500;
-        const annualRate = 6;
-        const years = 7;
-        const compoundsPerYear = 12;
-        const variance = 3;
+  test("calculate with variance below", () => {
+    const variance_below = calculateVariance(
+      principal,
+      annualRate,
+      compoundsPerYear,
+      years,
+      variance
+    ).total_below;
+    expect(variance_below).toBe(3083.387);
+  });
 
-        const variance_above = calculateVariance(principal,
-                                                 annualRate,
-                                                 compoundsPerYear,
-                                                 years,
-                                                 variance).total_above
-        expect(variance_above).toBe(4683.005);
-    })
+  test("should not accept negative variance", () => {
+    const variance = -3;
 
-   test("calculate with variance below", () => {
-        const principal = 2500;
-        const annualRate = 6;
-        const years = 7;
-        const compoundsPerYear = 12;
-        const variance = 3;
+    const calculate_variance = () => {
+      calculateVariance(
+        principal,
+        annualRate,
+        compoundsPerYear,
+        years,
+        variance
+      );
+    };
 
-        const variance_below = calculateVariance(principal, 
-                                                 annualRate,
-                                                 compoundsPerYear,
-                                                 years, 
-                                                 variance).total_below
-        expect(variance_below).toBe(3083.387);
-    });
-
-    test("should not accept negative variance", () => {
-        const principal = 2500;
-        const annualRate = 6;
-        const years = 7;
-        const compoundsPerYear = 12;
-        const variance = -3;
-
-        const calculate_variance = () => 
-                    {calculateVariance(principal, 
-                        annualRate,
-                        compoundsPerYear,
-                        years, 
-                        variance)}
-
-        expect(calculate_variance).toThrow(RangeError)
-    });
+    expect(calculate_variance).toThrow(RangeError);
+  });
 });
